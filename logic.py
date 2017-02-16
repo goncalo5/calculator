8300d49175ca27f1bd180d64f4a16256ef5a229e
goncalo andrade <goncalo@Joaos-MacBook-Pro.local>
2017-02-16 10:12:32 +0000

    mount add_value and evaluation count methods in logic file

--- logic.py
+++ logic.py
@@ -1,13 +1,11 @@
-def add_number(number):
-    print number
-
-
 class Count(object):
     def __init__(self, count=''):
         self.count = count
 
-    def add_number(self, number):
-        print number
+    def add_value(self, value):
+        self.count += str(value)
 
     def evaluate_count(self):
         print self.count
+        print eval(self.count)
+        self.count = ''
